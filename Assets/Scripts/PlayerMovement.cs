b86bcb1da76f92c70eda83051680e889fa3c631a
Daniel Barradas <66569532+barradasdan@users.noreply.github.com>
2025-01-22 13:16:31 -0300

    Movement Enhanced
    Setting better the movement:
    Now the player flips to the side that he is moving to.
    Also I've created a new variable that records the las side that player commanded, to not reset while player moves vertically. Camera was also attached to idle.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,13 @@ public class PlayerMovement : MonoBehaviour
 
     public float moveSpeed;
     Rigidbody2D rb;
-    Vector2 moveDir;
+
+    [HideInInspector]
+    public float lastHorizontalVector;
+    [HideInInspector]
+    public float lastVerticalVector;
+    [HideInInspector]
+    public Vector2 moveDir;
 
     void Start()
     {
@@ -28,6 +34,15 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDir = new Vector2(moveX, moveY).normalized;
+
+        if(moveDir.x != 0)
+        {
+            lastHorizontalVector = moveDir.x;
+        }
+        if(moveDir.y != 0)
+        {
+            lastVerticalVector = moveDir.y;
+        }
     }
 
     void Move()
