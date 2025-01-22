8c11ac9266cd9047f1e5176f94177237775ee1bc
Daniel Barradas <66569532+barradasdan@users.noreply.github.com>
2025-01-21 23:22:24 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    Rigidbody2D rb;
+    Vector2 moveDir;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void ImputManagement()
+    {
+
+    }
+
+    void Move()
+    {
+        
+    }
+
+}
